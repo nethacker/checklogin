d9daee7da9ce77890b7abc1f42cee85817781878
Phil Chen <nethacker@gmail.com>
2019-06-19 10:17:27 -0700

    checklogin shell script

---
+++ checklogin.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+#
+#The Below Directory Path is where the script will keep track of logins 
+BASE=/var/log/logins
+EMAIL=youremail@whatever.com
+#
+# The two files below checked for a delta against each other 
+HISTORY=${BASE}/history
+CURRENT=${BASE}/current
+#
+# Failure Function
+fail()
+{
+echo "Failed: $*"
+exit 1
+}
+#
+# Function to clean output from the last command
+clean_last()
+{
+/usr/bin/last | sed '{
+ /^reboot /d
+/^$/d
+/^wtmp begins /d
+}'
+}
+MYGROUP=`id -gn`
+MYIDENT=`id -un`
+#
+# Checking the env or error
+[ -d ${BASE} ] || mkdir -p ${BASE}
+[ -d ${BASE} ] || fail could not create ${BASE}
+[ -G ${BASE} ] || fail ${BASE} not owned by ${MYGROUP}
+[ -O ${BASE} ] || fail ${BASE} not owned by ${MYIDENT}
+#
+# Store current info
+clean_last >${CURRENT}
+# Is there a history file?
+if [ -f ${HISTORY} ]
+then
+#
+if ! `cmp --silent $CURRENT $HISTORY`
+then
+# Yes mail someone
+#
+diff $HISTORY $CURRENT |mail $EMAIL -s "Login report"
+fi
+fi
+#
+# Make current history
+#
+mv ${CURRENT} ${HISTORY}
+[ $? -eq 0 ] || fail mv ${CURRENT} ${HISTORY}
+exit 0
+ 
+#END OF SCRIPT
